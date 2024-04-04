cb0a09eb06e9d8082d3a2c4f24ba4417f42409c3
Shovon-07 <aljubair.shovon.07@gmail.com>
2024-04-04 06:37:34 +0600

    Comment

---
+++ E-Commerce/Project-Saudi/E-Commerce-Saudi-Backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Images;
+use App\Models\User;
+use Hash;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    function UserData(Request $request) {
+        $userData = User::all();
+        return $userData;
+    }
+    function CreateUser(Request $request) {
+        User::create([
+        'Name' => $request->input('name'),
+        'Email' => $request->input('email'),
+        'Password' => Hash::make($request->input('password'))
+        ]);
+        return "User created";
+    }
+
+    function GetImage(Request $request) {
+        return response()->json([
+            Images::all()
+        ]);
+    }
+    function CreateImage(Request $request) {
+        if($request->has("image")) {
+            $image = $request->file("image");
+            $imageName = time().".".$image->getClientOriginalName();
+            $image->move("images/",$imageName);
+            Images::create([
+                "Image"=>$imageName,
+            ]);
+            return response()->json([
+                "status"=>1,
+                "msg"=>"Image uploaded"
+            ]);
+        } else {
+            return response()->json([
+                "status"=>0,
+                "msg"=>"Please try again !"
+            ]);
+        }
+    }
+}
---
+++ E-Commerce/Project-Saudi/E-Commerce-Saudi/src/Pages/Sell/Sell.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+//___ Css ___//
+import "./Sell.scss";
+
+//___ Components ___//
+import Invoice from "../../Components/Invoice/Invoice";
+
+const Sell = () => {
+  return (
+    <div className="Sell">
+      <Invoice />
+    </div>
+  );
+};
+
+export default Sell;
