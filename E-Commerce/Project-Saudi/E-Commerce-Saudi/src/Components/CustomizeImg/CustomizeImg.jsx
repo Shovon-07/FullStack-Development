9c0d2133484425ca3003636ce473e5a6da1d0b36
Shovon-07 <aljubair.shovon.07@gmail.com>
2024-04-04 20:27:33 +0600

    C

--- E-Commerce/Project-Saudi/E-Commerce-Saudi/src/Components/CustomizeImg/CustomizeImg.jsx
+++ E-Commerce/Project-Saudi/E-Commerce-Saudi/src/Components/CustomizeImg/CustomizeImg.jsx
@@ -1,26 +1,34 @@
 import React, { useState } from "react";
+import Tooltip from "@mui/material/Tooltip";
 
 //___ Images ___//
 import jubba from "/images/dress/jubba2.png";
 
-import Btn_1 from "/images/dress/button_type/Button curve.png";
-import Btn_2 from "/images/dress/button_type/Buttoned square.png";
-import Btn_3 from "/images/dress/button_type/Buttoned triangle.png";
-import Btn_4 from "/images/dress/button_type/Hidden Zipper Curved.png";
-import Btn_5 from "/images/dress/button_type/Hidden zipped square.png";
-import Btn_6 from "/images/dress/button_type/Hidden zipper triangle.png";
+import Btn_1 from "/images/dress/button_type/Button Triangle.png";
+import Btn_2 from "/images/dress/button_type/Button Square.png";
+import Btn_3 from "/images/dress/button_type/Button Curved.png";
+import Btn_4 from "/images/dress/button_type/Hidden Curved.png";
+import Btn_5 from "/images/dress/button_type/Hidden Square.png";
+import Btn_6 from "/images/dress/button_type/Hidden Triangle.png";
+import Btn_7 from "/images/dress/button_type/Zipper Cuved.png";
+import Btn_8 from "/images/dress/button_type/Zipper Square.png";
+import Btn_9 from "/images/dress/button_type/Zipper Triangle.png";
 
 import Nack_1 from "/images/dress/neck_type/French.png";
-import Nack_2 from "/images/dress/neck_type/Curve.png";
-import Nack_3 from "/images/dress/neck_type/Royal Opened.png";
-import Nack_4 from "/images/dress/neck_type/Royal.png";
-import Nack_5 from "/images/dress/neck_type/Standard.png";
-import Nack_6 from "/images/dress/neck_type/Standard-2.png";
+import Nack_2 from "/images/dress/neck_type/double button.png";
+import Nack_3 from "/images/dress/neck_type/Closed Royal.png";
+import Nack_4 from "/images/dress/neck_type/Opened.png";
+import Nack_5 from "/images/dress/neck_type/Royal.png";
+import Nack_6 from "/images/dress/neck_type/Single Button.png";
 
-import Pocket_1 from "/images/dress/pocket_type/Curved standard.jpg";
-import Pocket_2 from "/images/dress/pocket_type/Curved.jpg";
-import Pocket_3 from "/images/dress/pocket_type/Triangle.jpg";
-import Pocket_4 from "/images/dress/pocket_type/Pointed.jpg";
+import Pocket_1 from "/images/dress/pocket_type/Pointed Triangle.png";
+import Pocket_2 from "/images/dress/pocket_type/Pointed.png";
+import Pocket_3 from "/images/dress/pocket_type/Hidden Curved.png";
+import Pocket_4 from "/images/dress/pocket_type/Curved.png";
+import Pocket_5 from "/images/dress/pocket_type/Square.png";
+import Pocket_6 from "/images/dress/pocket_type/Standard.png";
+import Pocket_7 from "/images/dress/pocket_type/Standard Curved.png";
+import Pocket_8 from "/images/dress/pocket_type/Standard Triangle.png";
 
 import Hand_1 from "/images/dress/hand_type/Normal Cuffed.png";
 import Hand_2 from "/images/dress/hand_type/Cuffed.png"; // img size w: 130 px; h:50 px direction <--
@@ -39,6 +47,8 @@ const CustomizeImg = () => {
   const [btnImgPreview, setBtnImgPreview] = useState({
     btnImgPrev: null,
     widthBtnImgPrev: null,
+    heightBtnImgPrev: null,
+    topBtnImgPrev: null,
     rightPosBtnImgPrev: null,
     leftPosBtnImgPrev: null,
   });
@@ -61,26 +71,126 @@ const CustomizeImg = () => {
   });
 
   const buttonImg = [
-    { btnImg: Btn_1, width: "16px", right: "139px" },
-    { btnImg: Btn_2, width: "16px", right: "139px" },
-    { btnImg: Btn_3, width: "16px", right: "139px" },
-    { btnImg: Btn_4, width: "16px", right: "139px" },
-    { btnImg: Btn_5, width: "16px", right: "139px" },
-    { btnImg: Btn_6, width: "16px", right: "139px" },
+    {
+      btnImg: Btn_1,
+      btnName: "Button Triangle",
+      width: "73px",
+      height: "170px",
+      top: "26px",
+      right: "110px",
+    },
+    {
+      btnImg: Btn_2,
+      btnName: "Button Square",
+      width: "73px",
+      height: "170px",
+      top: "26px",
+      right: "110px",
+    },
+    {
+      btnImg: Btn_3,
+      btnName: "Button Curved",
+      width: "73px",
+      height: "170px",
+      top: "26px",
+      right: "110px",
+    },
+    {
+      btnImg: Btn_4,
+      btnName: "Hidden Curved",
+      width: "71px",
+      height: "171px",
+      top: "16px",
+      right: "111px",
+    },
+    {
+      btnImg: Btn_5,
+      btnName: "Hidden Square",
+      width: "73px",
+      height: "170px",
+      top: "26px",
+      right: "110px",
+    },
+    {
+      btnImg: Btn_6,
+      btnName: "Hidden Triangle",
+      width: "73px",
+      height: "170px",
+      top: "26px",
+      right: "110px",
+    },
+    {
+      btnImg: Btn_7,
+      btnName: "Zipper Cuved",
+      width: "73px",
+      height: "170px",
+      top: "26px",
+      right: "110px",
+    },
+    {
+      btnImg: Btn_8,
+      btnName: "Zipper Square",
+      width: "73px",
+      height: "170px",
+      top: "26px",
+      right: "110px",
+    },
+    {
+      btnImg: Btn_9,
+      btnName: "Zipper Triangle",
+      width: "73px",
+      height: "170px",
+      top: "26px",
+      right: "110px",
+    },
   ];
   const nackImg = [
-    { nakImg: Nack_1, width: "80px", right: "108px" },
-    { nakImg: Nack_2, width: "80px", right: "108px" },
-    { nakImg: Nack_3, width: "80px", right: "108px" },
-    { nakImg: Nack_4, width: "80px", right: "108px" },
-    { nakImg: Nack_5, width: "80px", right: "108px" },
-    { nakImg: Nack_6, width: "80px", right: "108px" },
+    { nakImg: Nack_1, nakName: "French", width: "80px", right: "108px" },
+    {
+      nakImg: Nack_2,
+      nakName: "double button",
+      width: "80px",
+      right: "108px",
+    },
+    { nakImg: Nack_3, nakName: "Closed Royal", width: "80px", right: "108px" },
+    { nakImg: Nack_4, nakName: "Opened", width: "80px", right: "108px" },
+    { nakImg: Nack_5, nakName: "Royal", width: "80px", right: "108px" },
+    {
+      nakImg: Nack_6,
+      nakName: "Single Button ",
+      width: "80px",
+      right: "108px",
+    },
   ];
   const pocketImg = [
-    { pktImg: Pocket_1, width: "50px", right: "74px" },
-    { pktImg: Pocket_2, width: "50px", right: "74px" },
-    { pktImg: Pocket_3, width: "50px", right: "74px" },
-    { pktImg: Pocket_4, width: "50px", right: "74px" },
+    {
+      pktImg: Pocket_1,
+      pktName: "Pointed Triangle",
+      width: "58px",
+      right: "72px",
+    },
+    { pktImg: Pocket_2, pktName: "Pointed", width: "58px", right: "72px" },
+    {
+      pktImg: Pocket_3,
+      pktName: "Hidden Curved",
+      width: "58px",
+      right: "72px",
+    },
+    { pktImg: Pocket_4, pktName: "Curved", width: "58px", right: "72px" },
+    { pktImg: Pocket_5, pktName: "Square", width: "58px", right: "72px" },
+    { pktImg: Pocket_6, pktName: "Standard", width: "58px", right: "72px" },
+    {
+      pktImg: Pocket_7,
+      pktName: "Standard Curved",
+      width: "58px",
+      right: "72px",
+    },
+    {
+      pktImg: Pocket_8,
+      pktName: "Standard Triangle",
+      width: "58px",
+      right: "72px",
+    },
   ];
   const handImg = [
     { hndImg: Hand_1 },
@@ -102,6 +212,8 @@ const CustomizeImg = () => {
       ...btnImgPreview,
       btnImgPrev: items.btnImg,
       widthBtnImgPrev: items.width,
+      heightBtnImgPrev: items.height,
+      topBtnImgPrev: items.top,
       rightPosBtnImgPrev: items.right,
     });
   };
@@ -177,6 +289,8 @@ const CustomizeImg = () => {
             alt=""
             style={{
               width: btnImgPreview.widthBtnImgPrev,
+              height: btnImgPreview.heightBtnImgPrev,
+              top: btnImgPreview.topBtnImgPrev,
               right: btnImgPreview.rightPosBtnImgPrev,
             }}
             className={`${btnImgPreview == null ? "d-none" : "buttonImg"}`}
@@ -231,11 +345,19 @@ const CustomizeImg = () => {
               {buttonImg.map((items, index) => {
                 return (
                   <li key={index}>
-                    <img
+                    <Tooltip title={items.btnName}>
+                      <img
+                        src={items.btnImg}
+                        alt=""
+                        onClick={() => handelButtonImgPreview(items)}
+                      />
+                    </Tooltip>
+                    {/* <img
                       src={items.btnImg}
                       alt=""
                       onClick={() => handelButtonImgPreview(items)}
                     />
+                    <p>{items.btnName}</p> */}
                   </li>
                 );
               })}
@@ -249,11 +371,13 @@ const CustomizeImg = () => {
               {nackImg.map((items, index) => {
                 return (
                   <li key={index}>
-                    <img
-                      src={items.nakImg}
-                      alt=""
-                      onClick={() => handelNackImgPreview(items)}
-                    />
+                    <Tooltip title={items.nakName}>
+                      <img
+                        src={items.nakImg}
+                        alt=""
+                        onClick={() => handelNackImgPreview(items)}
+                      />
+                    </Tooltip>
                   </li>
                 );
               })}
@@ -267,11 +391,13 @@ const CustomizeImg = () => {
               {pocketImg.map((items, index) => {
                 return (
                   <li key={index}>
-                    <img
-                      src={items.pktImg}
-                      alt=""
-                      onClick={() => handelPocketImgPreview(items)}
-                    />
+                    <Tooltip title={items.pktName}>
+                      <img
+                        src={items.pktImg}
+                        alt=""
+                        onClick={() => handelPocketImgPreview(items)}
+                      />
+                    </Tooltip>
                   </li>
                 );
               })}
