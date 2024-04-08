0e2875d8f329b7639ab6ddf8e6eba7bf94e1a930
Shovon-07 <aljubair.shovon.07@gmail.com>
2024-04-08 22:08:56 +0600

    C

--- E-Commerce/Project-Saudi/E-Commerce-Saudi/src/Pages/InvoiceRecipt/InvoiceRecipt.jsx
+++ E-Commerce/Project-Saudi/E-Commerce-Saudi/src/Pages/InvoiceRecipt/InvoiceRecipt.jsx
@@ -19,6 +19,43 @@ const InvoiceRecipt = () => {
           <h2>Company name</h2>
         </div>
       </div>
+
+      <hr />
+      <div className="title">
+        <h2>Invoice</h2>
+      </div>
+
+      <div className="customerDetails d-flex flex-start gap-30">
+        <div className="left">
+          <table>
+            <tbody>
+              <tr>
+                <td>Name</td>
+                <td className="tdColon">:</td>
+                <td>Al jubair shovon</td>
+              </tr>
+              <tr>
+                <td>Phone</td>
+                <td className="tdColon">:</td>
+                <td>01767692422</td>
+              </tr>
+              <tr>
+                <td>Address</td>
+                <td className="tdColon">:</td>
+                <td>
+                  Charkhutar mor, Rajshahi court 6201, kashiadanga, Rajshahi.
+                  Charkhutar mor, Rajshahi court 6201, kashiadanga, Rajshahi.
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="right">
+          <p>Invoice # 10203</p>
+          <p>Issue date : 01/01/2024</p>
+          <p>Delivery : 10/01/2024</p>
+        </div>
+      </div>
     </div>
   );
 };
