a1112030aee0b38de0e5c6287216b6057f13b2d3
Shovon-07 <aljubair.shovon.07@gmail.com>
2024-04-15 16:29:20 +0600

    C

--- E-Commerce/Project-Saudi/E-Commerce-Saudi/src/assets/AxiosConfig.js
+++ E-Commerce/Project-Saudi/E-Commerce-Saudi/src/assets/AxiosConfig.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 export default function AxiosConfig() {
   const http = axios.create({
-    baseURL: "http://shovon-portfolio.wuaze.com/public/api",
+    baseURL: "http://shovon-portfolio.wuaze.com/Backend/public/api",
     headers: {
       "Content-type": "application/json",
+      // "Access-Control-Allow-Origin": "Access-Control-Allow-Origin",
     },
   });
   return {
