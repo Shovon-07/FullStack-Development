bd3adcdd4697d7689324a352f9d17dcedfc258c5
Shovon-07 <aljubair.shovon.07@gmail.com>
2024-06-29 22:54:34 +0600

    C

--- React_Laravel_Project/ReactJS/src/Pages/ToDo-App/ToDo_App.jsx
+++ React_Laravel_Project/ReactJS/src/Pages/ToDo-App/ToDo_App.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
+import { useRef, useState } from "react";
+import ToDo_Items from "./ToDo_Items";
 
 //___ Css ___//
 import "./ToDo_App.css";
 
-const ToDo_App = () => {
-  return (
-    <div className="ToDo_App">
-      <h3 className="pageTitle">ToDo App</h3>
-    </div>
-  )
-}
+//___ Images ___//
+import ToDo_Icon from "../../assets/Images/Icons/todo_icon.png";
 
-export default ToDo_App
+const ToDo_App = () => {
+  const inputRef = useRef();
+  const [toDoList, setToDoList] = useState([]);
+
+  const Add = () => {
+    const inputTxt = inputRef.current.value.trim();
+    if (inputTxt === "") {
+      return null;
+    }
+    const newToDo = {
+      id: Date.now(),
+      text: inputTxt,
+      isComplete: false,
+    };
+    setToDoList((prev) => [...prev, newToDo]);
+    inputRef.current.value = "";
+  };
+
+  return (
+    <div className="ToDo_App  d-flex">
+      <h3 className="pageTitle">ToDo App</h3>
+      <div className="todoBox">
+        <div className="d-flex gap-10">
+          <div>
+            <img src={ToDo_Icon} alt="" className="todoIcon" />
+          </div>
+          <div>
+            <h3 className="toDoTitle">To-Do List</h3>
+          </div>
+        </div>
+        <div className="addTask d-flex">
+          <input type="text" ref={inputRef} placeholder="Add your task" />
+          <button onClick={Add}>Add</button>
+        </div>
+        <div>
+          {toDoList.map((itemx, index) => {})}
+          <ToDo_Items text="Python" />
+          <ToDo_Items text="Java" />
+          <ToDo_Items text="Php" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ToDo_App;
---
+++ React_Laravel_Project/ReactJS/src/Pages/ToDo-App/ToDo_Items.jsx
@@ -0,0 +1,23 @@
+//___ Css ___//
+import "./ToDo_App.css";
+
+//___ Images ___//
+import Tick from "../../assets/Images/Icons/tick.png";
+import Not_Tick from "../../assets/Images/Icons/not_tick.png";
+import Delete from "../../assets/Images/Icons/delete.png";
+
+const ToDo_Items = (props) => {
+  const { text } = props;
+
+  return (
+    <div className="ToDo_Items d-flex gap-10">
+      <div className="toDoOnlyOneItems d-flex gap-10">
+        <img src={Tick} alt="" className="c_pointer" />
+        <p className="c_pointer">{text}</p>
+      </div>
+      <img src={Delete} alt="" className="deleteIcon c_pointer" />
+    </div>
+  );
+};
+
+export default ToDo_Items;
