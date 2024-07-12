22b83a7aa278602437b2fb64e9e978e461ea22a9
Shovon-07 <aljubair.shovon.07@gmail.com>
2024-07-12 19:49:20 +0600

    C

--- E-Commerce/Real_Estate/Frontend/src/Pages/ContactUs/ContactUs.jsx
+++ E-Commerce/Real_Estate/Frontend/src/Pages/ContactUs/ContactUs.jsx
@@ -1,33 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 
 //___ Css ___/
 import "./ContactUs.css";
 
 const ContactUs = () => {
+  const [input, setInput] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleInput = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="ContactUs page content">
       <h3 className="pageTitle">Contact us</h3>
       <div className="contactWrapper d-flex gap-20">
         <div className="left">1</div>
-        <div className="right">
-          <form>
+        <div className="right d-flex">
+          <form className="d-flex gap-30" onSubmit={handleSubmit}>
             <h3>Send message</h3>
             <div className="inputWrapper">
-              <label htmlFor="name">Name</label> <br />
-              <input type="text" id="name" placeholder="Enter your name" />
+              {/* <label htmlFor="name">Name</label> <br /> */}
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Enter your name"
+                onChange={handleInput}
+              />
             </div>
             <div className="inputWrapper">
-              <label htmlFor="email">Email</label> <br />
-              <input type="text" id="email" placeholder="Enter your email" />
+              {/* <label htmlFor="email">Email</label> <br /> */}
+              <input
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+                onChange={handleInput}
+              />
             </div>
             <div className="inputWrapper">
-              <label htmlFor="subject">Subject</label> <br />
-              <input type="text" id="subject" placeholder="Type subject" />
+              {/* <label htmlFor="subject">Subject</label> <br /> */}
+              <input
+                type="text"
+                id="subject"
+                name="subject"
+                placeholder="Type subject"
+                onChange={handleInput}
+              />
             </div>
             <div className="inputWrapper">
-              <label htmlFor="message">Message</label> <br />
-              <textarea name="" id="message" placeholder="Message"></textarea>
+              {/* <label htmlFor="message">Message</label> <br /> */}
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Message"
+                onChange={handleInput}
+              ></textarea>
             </div>
+            <button type="submit" className="btn">
+              send
+            </button>
           </form>
         </div>
       </div>
