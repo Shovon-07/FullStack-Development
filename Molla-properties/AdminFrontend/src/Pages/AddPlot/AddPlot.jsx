e2feaec3a9eaf5cae4283bfc3c21b0699bf5bd01
Shovon-07 <aljubair.shovon.07@gmail.com>
2024-07-25 20:31:04 +0600

    C

--- Molla-properties/AdminFrontend/src/Pages/AddPlot/AddPlot.jsx
+++ Molla-properties/AdminFrontend/src/Pages/AddPlot/AddPlot.jsx
@@ -4,52 +4,59 @@ import { useState } from "react";
 import "./AddPlot.css";
 
 const AddPlot = () => {
-  const [sectionCounter, setSectionCounter] = useState([1]);
+  const [inputList, setinputList] = useState([{ plot: "" }]);
 
-  const AddPlotInputField = () => {
-    setSectionCounter([
-      ...sectionCounter,
-      sectionCounter.push(1 * Math.random()),
-    ]);
+  const handleinputchange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setinputList(list);
   };
 
-  const RemovePlotInputField = () => {
-    if (sectionCounter.length == 1) {
+  const handleremove = () => {
+    if (inputList.length == 1) {
       return;
     } else {
-      setSectionCounter(sectionCounter.slice(0, -1));
+      setinputList(inputList.slice(0, -1));
     }
+  };
+
+  const handleaddclick = () => {
+    setinputList([...inputList, { plot: "" }]);
   };
 
   return (
     <div className="AddPlot">
       <h3 className="pageTitle">Add plot</h3>
-      <div className="plotInput">
-        {sectionCounter.map((items, index) => {
-          return (
-            <div className="inputWrapper" key={index}>
-              <input type="text" placeholder={`Plot ${index + 1}`} />
-            </div>
-          );
-        })}
-        <div className="addRemBtn d-flex gap-30">
-          <button
-            type="button"
-            className="minus"
-            style={{ background: "#ec0202", padding: "0 15px" }}
-            onClick={RemovePlotInputField}
-          >
-            -
-          </button>
-          <button
-            type="button"
-            className="plus"
-            style={{ background: "#029802", padding: "0 10px" }}
-            onClick={AddPlotInputField}
-          >
-            +
-          </button>
-        </div>
+      {inputList.map((x, i) => {
+        return (
+          <div className="inputWrapper" key={i}>
+            <input
+              type="text"
+              name="plot"
+              placeholder={`Plot ${i + 1}`}
+              onChange={(e) => handleinputchange(e, i)}
+            />
+          </div>
+        );
+      })}
+      <div className="addRemBtn d-flex gap-30">
+        <button
+          type="button"
+          className="minus"
+          style={{ background: "#ec0202", padding: "0 15px" }}
+          onClick={handleremove}
+        >
+          -
+        </button>
+        <button
+          type="button"
+          className="plus"
+          style={{ background: "#029802", padding: "0 10px" }}
+          onClick={handleaddclick}
+        >
+          +
+        </button>
       </div>
     </div>
   );
