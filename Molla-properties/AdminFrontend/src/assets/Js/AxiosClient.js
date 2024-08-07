4a41935b2aa92e8af1734ab6b5a57becde4f4919
Shovon-07 <aljubair.shovon.07@gmail.com>
2024-08-07 09:54:52 +0600

    C

--- Molla-properties/AdminFrontend/src/assets/Js/AxiosClient.js
+++ Molla-properties/AdminFrontend/src/assets/Js/AxiosClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const AxiosClient = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/admin",
-  // baseURL: "https://backend.molla-properties.com/public/api/admin",
+  // baseURL: "http://127.0.0.1:8000/api/admin",
+  baseURL: "https://backend.molla-properties.com/public/api/admin",
   headers: {
     "Content-type": "multipart/form-data",
     API_KEY: "83b6349651735fb8b3c6b20b1bc882ba",
