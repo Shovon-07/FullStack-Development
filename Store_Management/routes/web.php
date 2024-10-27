6c67955e843ef9901984438d3097a3888aaab5ab
Shovon-07 <aljubair.shovon.07@gmail.com>
2024-10-27 22:17:52 +0600

    Comment

--- Store_Management/routes/web.php
+++ Store_Management/routes/web.php
@@ -6,7 +6,7 @@ use function Termwind\render;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return inertia('Home');
+    return inertia('Home/Home');
 });
 Route::get('/about', function () {
     return Inertia::render('About/About');
