3351dcf5162f57a88564da6d3a229e5e9e56e9bb
Shovon-07 <aljubair.shovon.07@gmail.com>
2025-01-18 23:09:21 +0600

    Committed some changes

--- MERN/Server/Util/DB.js
+++ MERN/Server/Util/DB.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const config = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "ChatApp",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 config.connect((err) => {
--- MERN/Server/server.js
+++ MERN/Server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = 3000;
@@ -7,17 +8,17 @@ app.get("/", (req, res) => {
   res.send("This is backend");
 });
 
-// app.get("/user", (req, res) => {
-//   DB.query("SELECT * FROM users", (err, result, field) => {
-//     if (err) {
-//       console.log(err);
-//       return;
-//     } else {
-//       console.log(result);
-//       res.status(200).json({ data: result });
-//     }
-//   });
-// });
+app.get("/user", (req, res) => {
+  DB.query("SELECT * FROM users", (err, result, field) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log(result);
+      res.status(200).json({ data: result });
+    }
+  });
+});
 
 //==> Run server
 app.listen(PORT, () => {
