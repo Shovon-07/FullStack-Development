88f5bad4eced20eff8f660eab56edf3dac496f60
Shovon-07 <aljubair.shovon.07@gmail.com>
2025-01-19 23:35:39 +0600

    Committed some changes

--- ChatApp/Server/Controllers/AuthController.js
+++ ChatApp/Server/Controllers/AuthController.js
@@ -1,17 +1,6 @@
 const DB = require("../Util/DB");
-
-const index = async (req, res) => {
-  try {
-    DB.query("SELECT * FROM users", (err, result, field) => {
-      if (err) {
-        console.log(err);
-      }
-      return res.status(200).json({ status: true, data: result });
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
+const bcrypt = require("bcrypt");
+const saltRounds = process.env.BCRYP_SALT_ROUNDS;
 
 const Login = async (req, res) => {
   try {
@@ -20,20 +9,22 @@ const Login = async (req, res) => {
       [req.body.email, req.body.password],
       (err, result, field) => {
         if (err) {
-          console.log(err);
-        }
-
-        if (result.length != 0) {
-          return res
-            .status(200)
-            .json({ status: true, msg: "Login sucessfull", data: result });
+          return res.status(400).json({ status: false, msg: err });
         } else {
-          return res.status(200).json({ status: false, msg: "User not found" });
+          if (result.length != 0) {
+            return res
+              .status(200)
+              .json({ status: true, msg: "Login sucessfull", data: result });
+          } else {
+            return res
+              .status(200)
+              .json({ status: false, msg: "User not found" });
+          }
         }
       }
     );
   } catch (err) {
-    console.log(err);
+    return res.status(400).json({ status: false, msg: err });
   }
 };
 
@@ -45,34 +36,72 @@ const SignUp = async (req, res) => {
       req.body.email,
       (err, result, field) => {
         if (err) {
-          console.log(err);
-        }
-
-        let userExist = result;
-        // Store data
-        if (userExist <= 0) {
-          DB.query(
-            "INSERT INTO users SET ?",
-            req.body,
-            (err, result, field) => {
-              if (err) {
-                console.log(err);
-              }
-              return res
-                .status(200)
-                .json({ status: true, msg: "Registration successfull" });
-            }
-          );
+          return res.status(400).json({ status: false, msg: err });
         } else {
-          return res
-            .status(200)
-            .json({ status: false, msg: "User already exist" });
+          // Store data
+          if (result <= 0) {
+            const { name, email, password } = req.body;
+            DB.query(
+              `INSERT INTO users (name, email, password) VALUES ('${name}', '${email}', '${password}')`,
+              (err, result, field) => {
+                if (err) {
+                  return res.status(400).json({ status: false, msg: err });
+                } else {
+                  return res
+                    .status(200)
+                    .json({ status: true, msg: "Registration successfull" });
+                }
+              }
+            );
+          } else {
+            return res
+              .status(200)
+              .json({ status: false, msg: "User already exist" });
+          }
         }
       }
     );
   } catch (err) {
-    console.log(err);
+    return res.status(400).json({ status: false, msg: err });
   }
 };
 
-module.exports = { index, Login, SignUp };
+// const SignUp = async (req, res) => {
+//   try {
+//     // Check duplicate user
+//     DB.query(
+//       "SELECT email FROM users WHERE email = ?",
+//       req.body.email,
+//       (err, result, field) => {
+//         if (err) {
+//           return res.status(400).json({ status: false, msg: err });
+//         } else {
+//           // Store data
+//           if (result <= 0) {
+//             DB.query(
+//               "INSERT INTO users SET ?",
+//               req.body,
+//               (err, result, field) => {
+//                 if (err) {
+//                   return res.status(400).json({ status: false, msg: err });
+//                 } else {
+//                   return res
+//                     .status(200)
+//                     .json({ status: true, msg: "Registration successfull" });
+//                 }
+//               }
+//             );
+//           } else {
+//             return res
+//               .status(200)
+//               .json({ status: false, msg: "User already exist" });
+//           }
+//         }
+//       }
+//     );
+//   } catch (err) {
+//     return res.status(400).json({ status: false, msg: err });
+//   }
+// };
+
+module.exports = { Login, SignUp };
--- ChatApp/Server/Routes/auth-route.js
+++ ChatApp/Server/Routes/auth-route.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const AuthController = require("../Controllers/AuthController");
 
-router.route("/").post(AuthController.index);
 router.route("/login").post(AuthController.Login);
 router.route("/sign-up").post(AuthController.SignUp);
 
