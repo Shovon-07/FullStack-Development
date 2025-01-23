b7b30fc2b6567c869d8daef6baabef1949a8f2e4
Shovon-07 <aljubair.shovon.07@gmail.com>
2025-01-23 22:10:34 +0600

    Committed some changes

--- ChatApp/Client/src/pages/Home/Home.jsx
+++ ChatApp/Client/src/pages/Home/Home.jsx
@@ -1,21 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import { io } from "socket.io-client";
 
 const Home = () => {
   const socket = io("http://localhost:3001");
+  const [msg, setMsg] = useState("");
 
-  socket.on("connect", () => {
-    console.log(socket.id, " connected");
-  });
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log(socket.id, " connected");
+    });
 
-  socket.on("disconnect", () => {
-    console.log(socket.id, " disconnected");
-  });
+    socket.on("disconnect", () => {
+      console.log(socket.id, " disconnected");
+    });
+  }, []);
 
-  // useEffect(() => {
+  const Send = () => {
+    console.log(msg);
+    socket.emit("user_msg", msg);
+    setMsg("");
+  };
 
-  // }, []);
   return (
     <HelmetProvider>
       <Helmet>
@@ -33,6 +39,16 @@ const Home = () => {
 
       <div className="animated fadeInDown">
         <h1 className="page-title">Dashboard</h1>
+
+        <input
+          type="text"
+          placeholder="Type hear"
+          onChange={(e) => {
+            setMsg(e.target.value);
+          }}
+          value={msg}
+        />
+        <button onClick={Send}>Send</button>
       </div>
     </HelmetProvider>
   );
