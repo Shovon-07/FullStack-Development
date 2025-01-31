3c799d49bd13640cd1d16d308667ede3677d5af2
Shovon-07 <aljubair.shovon.07@gmail.com>
2025-01-31 22:32:57 +0600

    Committed some changes

--- PWA/LearnPwa/src/main.jsx
+++ PWA/LearnPwa/src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js").then(() => {
-    console.log("Service Worker Registered");
-  });
-}
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker.register("/sw.js").then(() => {
+//     console.log("Service Worker Registered");
+//   });
+// }
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
