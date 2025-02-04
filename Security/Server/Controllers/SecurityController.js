c64115fe89b900557dbd3f25879b2b46f20dfa53
Shovon-07 <aljubair.shovon.07@gmail.com>
2025-02-04 17:33:24 +0600

    Committed some changes

--- Security/Server/Controllers/SecurityController.js
+++ Security/Server/Controllers/SecurityController.js
@@ -2,8 +2,15 @@ const SecurityModel = require("../Models/SecurityModel");
 
 const GetSecurity = async (req, res) => {
   try {
-    const data = await SecurityModel.find();
-    return res.status(200).json(data);
+    const data = await SecurityModel.find({
+      domain: req.body.domain,
+      token: req.body.token,
+    });
+    if (data.length > 0) {
+      return res.status(200).json({ status: true, data: data });
+    } else {
+      return res.status(500).json({ status: false, data: "No data found" });
+    }
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
