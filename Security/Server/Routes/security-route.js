59b6f6470c776089f01451918a786974f13272fc
Shovon-07 <aljubair.shovon.07@gmail.com>
2025-02-05 11:11:33 +0600

    Committed some changes

--- Security/Server/Routes/security-route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const SecurityController = require("../Controllers/SecurityController");
-
-router.route("/get-security").post(SecurityController.GetSecurity);
-router.route("/update-security").post(SecurityController.UpdateSecurity);
-
-module.exports = router;
