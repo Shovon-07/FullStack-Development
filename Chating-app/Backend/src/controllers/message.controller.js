aa07a714dc9ab45286e3da97fa06670d220477c7
Shovon-07 <aljubair.shovon.07@gmail.com>
2025-02-10 19:43:48 +0600

    Committed some changes

--- Chating-app/Backend/src/controllers/message.controller.js
+++ Chating-app/Backend/src/controllers/message.controller.js
@@ -2,9 +2,16 @@ import User from "../models/user.model.js";
 
 export const getUserForSidebar = async (req, res) => {
   try {
-    const id = req.id;
-    const email = req.email;
-    return res.status(200).json({ status: true, data: id });
+    const loggedInUserId = req.id;
+    // const loggedInUserEmail = req.email;
+
+    //===> Find all user without me
+    const users = await User.find(
+      { _id: { $ne: loggedInUserId } },
+      { password: 0 }
+    );
+
+    return res.status(200).json({ status: true, data: users });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ status: "error", message: error.message });
