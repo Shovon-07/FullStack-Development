2a61edac86b6126c842b698521452388ee0dc5e8
Shovon-07 <aljubair.shovon.07@gmail.com>
2025-02-13 17:42:54 +0600

    Committed some changes

--- Chating-app/Frontend/src/pages/Chat/Chat.jsx
+++ Chating-app/Frontend/src/pages/Chat/Chat.jsx
@@ -1,12 +1,211 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import demoImg from "../../assets/images/profile.png";
+
+//===> Css
+import "./Chat.css";
+
+//===> Icons
+import { IoIosSend } from "react-icons/io";
+import { FaRegFaceSmileBeam } from "react-icons/fa6";
+import { GrAttachment } from "react-icons/gr";
 
 const Chat = () => {
+  const { id } = useParams();
+
   return (
     <div className="Chat">
-      <div className="chatHead">head</div>
-      <div className="chatBox">chats</div>
+      <div className="chatHead">head - {id}</div>
+
+      <div className="chatBox">
+        <div className="chat chat-start">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS chat bubble component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <div className="chat-header">
+            Obi-Wan Kenobi<time className="text-xs opacity-50 ml-2">12:45</time>
+          </div>
+          <div className="chat-bubble">You were the Chosen One!</div>
+          <div className="chat-footer opacity-50">Delivered</div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img alt="Tailwind CSS chat bubble component" src={demoImg} />
+            </div>
+          </div>
+          <div className="chat-header">
+            Anakin
+            <time className="text-xs opacity-50 ml-2">12:46</time>
+          </div>
+          <div className="chat-bubble">I hate you!</div>
+          <div className="chat-footer opacity-50">Seen at 12:46</div>
+        </div>
+
+        <div className="chat chat-start">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS chat bubble component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <div className="chat-header">
+            Obi-Wan Kenobi<time className="text-xs opacity-50 ml-2">12:45</time>
+          </div>
+          <div className="chat-bubble">You were the Chosen One!</div>
+          <div className="chat-footer opacity-50">Delivered</div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img alt="Tailwind CSS chat bubble component" src={demoImg} />
+            </div>
+          </div>
+          <div className="chat-header">
+            Anakin
+            <time className="text-xs opacity-50 ml-2">12:46</time>
+          </div>
+          <div className="chat-bubble">I hate you!</div>
+          <div className="chat-footer opacity-50">Seen at 12:46</div>
+        </div>
+        <div className="chat chat-start">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS chat bubble component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <div className="chat-header">
+            Obi-Wan Kenobi<time className="text-xs opacity-50 ml-2">12:45</time>
+          </div>
+          <div className="chat-bubble">You were the Chosen One!</div>
+          <div className="chat-footer opacity-50">Delivered</div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img alt="Tailwind CSS chat bubble component" src={demoImg} />
+            </div>
+          </div>
+          <div className="chat-header">
+            Anakin
+            <time className="text-xs opacity-50 ml-2">12:46</time>
+          </div>
+          <div className="chat-bubble">I hate you!</div>
+          <div className="chat-footer opacity-50">Seen at 12:46</div>
+        </div>
+        <div className="chat chat-start">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS chat bubble component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <div className="chat-header">
+            Obi-Wan Kenobi<time className="text-xs opacity-50 ml-2">12:45</time>
+          </div>
+          <div className="chat-bubble">You were the Chosen One!</div>
+          <div className="chat-footer opacity-50">Delivered</div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img alt="Tailwind CSS chat bubble component" src={demoImg} />
+            </div>
+          </div>
+          <div className="chat-header">
+            Anakin
+            <time className="text-xs opacity-50 ml-2">12:46</time>
+          </div>
+          <div className="chat-bubble">I hate you!</div>
+          <div className="chat-footer opacity-50">Seen at 12:46</div>
+        </div>
+        <div className="chat chat-start">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS chat bubble component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <div className="chat-header">
+            Obi-Wan Kenobi<time className="text-xs opacity-50 ml-2">12:45</time>
+          </div>
+          <div className="chat-bubble">You were the Chosen One!</div>
+          <div className="chat-footer opacity-50">Delivered</div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img alt="Tailwind CSS chat bubble component" src={demoImg} />
+            </div>
+          </div>
+          <div className="chat-header">
+            Anakin
+            <time className="text-xs opacity-50 ml-2">12:46</time>
+          </div>
+          <div className="chat-bubble">I hate you!</div>
+          <div className="chat-footer opacity-50">Seen at 12:46</div>
+        </div>
+        <div className="chat chat-start">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS chat bubble component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <div className="chat-header">
+            Obi-Wan Kenobi<time className="text-xs opacity-50 ml-2">12:45</time>
+          </div>
+          <div className="chat-bubble">You were the Chosen One!</div>
+          <div className="chat-footer opacity-50">Delivered</div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img alt="Tailwind CSS chat bubble component" src={demoImg} />
+            </div>
+          </div>
+          <div className="chat-header">
+            Anakin
+            <time className="text-xs opacity-50 ml-2">12:46</time>
+          </div>
+          <div className="chat-bubble">I hate you!</div>
+          <div className="chat-footer opacity-50">Seen at 12:46</div>
+        </div>
+      </div>
+
       <div className="chatFoot">
-        <input type="text" placeholder="Type somethig" />
+        <div className="left">
+          <div className="imoji">
+            <FaRegFaceSmileBeam />
+          </div>
+          <div className="inputBox">
+            <input type="text" placeholder="Type somethig" />
+          </div>
+          <div className="attach">
+            <GrAttachment />
+          </div>
+        </div>
+        <div className="right">
+          <div className="send">
+            <IoIosSend size={25} />
+          </div>
+        </div>
       </div>
     </div>
   );
